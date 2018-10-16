513c12f1cf559b000b4844a980f5d8aba990a104
noel <noelberistain@gmail.com>
2018-10-16 16:54:08 -0700

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,14 +15,14 @@ io.on('connection', function (socket) {
 
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        createdAt: 123123
-    });
 
     socket.on('createMessage', function (message) {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('createEmail', function (newEmail) {
