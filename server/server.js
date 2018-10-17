397227edddbb8c773a606064615f05ef97a4fabb
noel <noelberistain@gmail.com>
2018-10-16 17:30:07 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -5,16 +5,26 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3001;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
-io.on('connection', function (socket) {
 
+io.on('connection', function (socket) {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', function (message) {
         console.log('createMessage', message);
@@ -25,19 +35,14 @@ io.on('connection', function (socket) {
         });
     });
 
-    socket.on('createEmail', function (newEmail) {
-        console.log('createEmail', newEmail);
-    });
-
     socket.on('disconnect', function () {
         console.log('User was disconnected');
-    })
+    });
 
 }); //end of -- io.on('connection', fun...
 
 server.listen(port, () => {
     console.log(`server is up on port ${port}`);
-
 });
 
 
