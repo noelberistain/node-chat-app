4b645d697fd97eeb8e6435bf02fc2f497616481d
noel <noelberistain@gmail.com>
2018-10-18 15:36:19 -0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,49 @@
+// var socket = io();
+
+// function scrollToBottom() {
+//     //Selectors
+//     var messages = jQuery('#messages');
+//     var newMessage = messages.children('li:last-child');
+//     //Heights
+//     var clientHeight = messages.prop('clientheight');
+//     console.log(messages.prop('clientheight'));
+//     var scrollTop = messages.prop('scrollTop');
+//     var scrollHeight = messages.prop('scrollHeight');
+//     var newMessageHeight = newMessage.innerHeight();
+//     var lastMessageHeight = newMessage.prev().innerHeight();
+
+//     if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+//         messages.scrollTop(scrollHeight);
+//     }
+// }
 var socket = io();
 
-socket.on('connect', function() {
+function scrollToBottom() {
+    //selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        // console.log("Should scroll");
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+socket.on("connect", function () {
     console.log('Connected to server');
 });
 
-socket.on('disconnect', function() {
+socket.on("disconnect", function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message) {
+socket.on("newMessage", function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     // console.log('New message', message);
     // var li = jQuery('<li></li>');
@@ -22,9 +57,10 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
-socket.on('newLocationMessage', function(message) {
+socket.on("newLocationMessage", function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a >');
@@ -39,11 +75,11 @@ socket.on('newLocationMessage', function(message) {
         url: message.url,
         createdAt: formattedTime
     });
-    console.log(html);
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
-jQuery('#message-form').on('submit', function(e) {
+jQuery('#message-form').on("submit", function (e) {
     e.preventDefault();
 
     var messageTextbox = jQuery('[name=message]');
@@ -51,27 +87,27 @@ jQuery('#message-form').on('submit', function(e) {
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextbox.val()
-    }, function() {
+    }, function () {
         messageTextbox.val('');
     })
 });
 
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function() {
+locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
 
     locationButton.attr('disabled', 'disabled').text('Sending location...');
 
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
         locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
-    }, function() {
+    }, function () {
         locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
