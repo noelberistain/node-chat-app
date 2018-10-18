957bfae851782e23b7daaecd63639a6da4d6d41a
noel <noelberistain@gmail.com>
2018-10-18 15:37:32 -0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,3 @@
-// var socket = io();
-
-// function scrollToBottom() {
-//     //Selectors
-//     var messages = jQuery('#messages');
-//     var newMessage = messages.children('li:last-child');
-//     //Heights
-//     var clientHeight = messages.prop('clientheight');
-//     console.log(messages.prop('clientheight'));
-//     var scrollTop = messages.prop('scrollTop');
-//     var scrollHeight = messages.prop('scrollHeight');
-//     var newMessageHeight = newMessage.innerHeight();
-//     var lastMessageHeight = newMessage.prev().innerHeight();
-
-//     if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-//         messages.scrollTop(scrollHeight);
-//     }
-// }
 var socket = io();
 
 function scrollToBottom() {
